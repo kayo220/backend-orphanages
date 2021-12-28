8e9f8efb58d2c0a77dd250d4317dd4a47f476aa5
Kayo Costa <kayo220@users.noreply.github.com>
2021-12-28 13:19:37 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors'
+import path from 'path';
+import cors from 'cors';
+import './database/connection'
+import routes from './routes'
+import errorHandler from './errors/handler'
+
+const app = express();
+app.use(cors(
+    //{origin:[]} pode pasar a orgem do frontend que fará a requisição
+));
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))//acesso à pasta de uploads
+app.get('/users', (request, response) => {
+    return response.json({ message: 'Hello World' })
+    //response.send("Hello World");
+});
+app.use(errorHandler);
+
+
+app.listen(3333);
+//localhost:3333
